869931ecae06a25acfa4195ba0e74fa47c33da23
joony.kim <bestheroz@gmail.com>
2024-12-26 13:59:16 +0900

    ♻️

--- src/main/kotlin/com/github/bestheroz/standard/common/util/LogUtils.kt
+++ src/main/kotlin/com/github/bestheroz/standard/common/util/LogUtils.kt
@@ -1,11 +1,12 @@
 package com.github.bestheroz.standard.common.util
 
 object LogUtils {
-    fun getStackTrace(throwable: Throwable): String =
-        throwable.stackTrace
-            .asSequence()
-            .filter { stackTraceElement ->
+    fun getStackTrace(throwable: Throwable?): String =
+        throwable
+            ?.stackTrace
+            ?.asSequence()
+            ?.filter { stackTraceElement ->
                 stackTraceElement.className.startsWith("com.github.bestheroz") ||
                     !stackTraceElement.toString().startsWith("\tat")
-            }.joinToString(separator = "\n") { it.toString() }
+            }?.joinToString(separator = "\n") { it.toString() } ?: ""
 }
