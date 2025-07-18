1407e1b284151f76842769d78e8e5784661ae65d
joony.kim <bestheroz@gmail.com>
2025-07-18 13:24:46 +0900

    ♻️

--- src/main/kotlin/com/github/bestheroz/demo/services/AdminService.kt
+++ src/main/kotlin/com/github/bestheroz/demo/services/AdminService.kt
@@ -27,6 +27,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional(readOnly = true)
 class AdminService(
     private val adminRepository: AdminRepository,
     private val operatorHelper: OperatorHelper,
