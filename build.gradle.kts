3b7e73a8e3a2587441609f5533f411e6bc42d48d
joony.kim <bestheroz@gmail.com>
2025-10-04 23:22:23 +0900

    ⬆️update packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,10 @@ plugins {
     kotlin("plugin.spring") version kotlinVersion
 
     id("com.google.devtools.ksp") version "2.2.20-2.0.3"
-    id("org.springframework.boot") version "3.5.5"
+    id("org.springframework.boot") version "3.5.6"
     id("io.spring.dependency-management") version "1.1.7"
-    id("com.diffplug.spotless") version "7.2.1"
-    id("com.github.ben-manes.versions") version "0.52.0"
+    id("com.diffplug.spotless") version "8.0.0"
+    id("com.github.ben-manes.versions") version "0.53.0"
     idea
 }
 
@@ -27,7 +27,7 @@ repositories {
 
 dependencyManagement {
     imports {
-        mavenBom("org.springframework.boot:spring-boot-dependencies:3.5.5")
+        mavenBom("org.springframework.boot:spring-boot-dependencies:3.5.6")
     }
 }
 
@@ -35,7 +35,7 @@ dependencies {
     // Kotlin
     implementation(kotlin("reflect"))
     implementation(kotlin("stdlib"))
-    ksp("com.google.dagger:dagger-compiler:2.57.1")
+    ksp("com.google.dagger:dagger-compiler:2.57.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     // Coroutines
@@ -56,11 +56,11 @@ dependencies {
     // Logging and Sentry
     implementation("com.auth0:java-jwt:4.5.0")
     implementation("io.github.oshai:kotlin-logging-jvm:7.0.13")
-    implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.21.1")
-    implementation("io.sentry:sentry-logback:8.21.1")
+    implementation("io.sentry:sentry-spring-boot-starter-jakarta:8.23.0")
+    implementation("io.sentry:sentry-logback:8.23.0")
 
     // OpenAPI (UI includes API dependency)
-    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:2.8.13")
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:3.0.0-M1")
 
     // Utility
     implementation("org.fusesource.jansi:jansi:2.4.2")
