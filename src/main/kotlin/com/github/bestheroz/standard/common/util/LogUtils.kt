69c82edb4db1e4e27803d770719ced85055c1e3e
joony.kim <bestheroz@gmail.com>
2026-01-03 17:51:19 +0900

    ♻️

--- src/main/kotlin/com/github/bestheroz/standard/common/util/LogUtils.kt
+++ src/main/kotlin/com/github/bestheroz/standard/common/util/LogUtils.kt
@@ -1,13 +1,11 @@
 package com.github.bestheroz.standard.common.util
 
 import org.apache.commons.lang3.exception.ExceptionUtils
-import java.util.Arrays
-import java.util.stream.Collectors
 
 object LogUtils {
     fun getStackTrace(e: Throwable?): String =
-        Arrays
-            .stream(ExceptionUtils.getStackFrames(e))
+        ExceptionUtils
+            .getStackFrames(e)
             .filter { it.startsWith("\tat com.github.bestheroz") || !it.startsWith("\tat") }
-            .collect(Collectors.joining("\n"))
+            .joinToString("\n")
 }
