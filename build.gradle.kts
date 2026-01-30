0090ea55aef588ad0dfaacfd7a23582fd6299741
joony.kim <bestheroz@gmail.com>
2026-01-30 23:48:50 +0900

    ⬆️update packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.gradle.kotlin.dsl.invoke
 
 plugins {
-    val kotlinVersion = "2.3.20-Beta1"
+    val kotlinVersion = "2.3.20-Beta2"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
 
-    id("com.google.devtools.ksp") version "2.3.4"
-    id("org.springframework.boot") version "4.0.1"
+    id("com.google.devtools.ksp") version "2.3.5"
+    id("org.springframework.boot") version "4.1.0-M1"
     id("io.spring.dependency-management") version "1.1.7"
-    id("com.diffplug.spotless") version "8.1.0"
+    id("com.diffplug.spotless") version "8.2.1"
     id("com.github.ben-manes.versions") version "0.53.0"
     idea
 }
@@ -27,7 +27,7 @@ repositories {
 
 dependencyManagement {
     imports {
-        mavenBom("org.springframework.boot:spring-boot-dependencies:4.0.1")
+        mavenBom("org.springframework.boot:spring-boot-dependencies:4.1.0-M1")
     }
 }
 
@@ -35,7 +35,7 @@ dependencies {
     // Kotlin
     implementation(kotlin("reflect"))
     implementation(kotlin("stdlib"))
-    ksp("com.google.dagger:dagger-compiler:2.58")
+    ksp("com.google.dagger:dagger-compiler:2.59")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     // Coroutines
@@ -55,8 +55,8 @@ dependencies {
     // Logging and Sentry
     implementation("com.auth0:java-jwt:4.5.0")
     implementation("io.github.oshai:kotlin-logging-jvm:8.0.00-beta-04")
-    implementation("io.sentry:sentry-spring-boot-4:8.30.0")
-    implementation("io.sentry:sentry-logback:8.30.0")
+    implementation("io.sentry:sentry-spring-boot-4:8.31.0")
+    implementation("io.sentry:sentry-logback:8.31.0")
 
     // OpenAPI (UI includes API dependency)
     implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:3.0.1")
