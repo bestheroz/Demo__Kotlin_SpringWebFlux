2c2254aaf3c030a12cad32396af75b094395ae8d
joony.kim <bestheroz@gmail.com>
2026-03-13 07:49:39 +0900

    ⬆️update packages

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
 import org.gradle.kotlin.dsl.invoke
 
 plugins {
-    val kotlinVersion = "2.3.20-Beta2"
+    val kotlinVersion = "2.3.20-RC3"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
 
-    id("com.google.devtools.ksp") version "2.3.5"
-    id("org.springframework.boot") version "4.1.0-M1"
+    id("com.google.devtools.ksp") version "2.3.6"
+    id("org.springframework.boot") version "4.1.0-M2"
     id("io.spring.dependency-management") version "1.1.7"
-    id("com.diffplug.spotless") version "8.2.1"
+    id("com.diffplug.spotless") version "8.3.0"
     id("com.github.ben-manes.versions") version "0.53.0"
     idea
 }
@@ -27,7 +27,7 @@ repositories {
 
 dependencyManagement {
     imports {
-        mavenBom("org.springframework.boot:spring-boot-dependencies:4.1.0-M1")
+        mavenBom("org.springframework.boot:spring-boot-dependencies:4.1.0-M2")
     }
 }
 
@@ -35,7 +35,7 @@ dependencies {
     // Kotlin
     implementation(kotlin("reflect"))
     implementation(kotlin("stdlib"))
-    ksp("com.google.dagger:dagger-compiler:2.59.1")
+    ksp("com.google.dagger:dagger-compiler:2.59.2")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     // Coroutines
@@ -55,11 +55,11 @@ dependencies {
     // Logging and Sentry
     implementation("com.auth0:java-jwt:4.5.1")
     implementation("io.github.oshai:kotlin-logging-jvm:8.0.01")
-    implementation("io.sentry:sentry-spring-boot-4:8.32.0")
-    implementation("io.sentry:sentry-logback:8.32.0")
+    implementation("io.sentry:sentry-spring-boot-4:8.35.0")
+    implementation("io.sentry:sentry-logback:8.35.0")
 
     // OpenAPI (UI includes API dependency)
-    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:3.0.1")
+    implementation("org.springdoc:springdoc-openapi-starter-webflux-ui:3.0.2")
 
     // Utility
     implementation("org.fusesource.jansi:jansi:2.4.2")
@@ -76,6 +76,10 @@ tasks.withType<JavaCompile> {
 
 tasks.bootJar {
     archiveFileName.set("demo.jar")
+}
+
+tasks.bootRun {
+    jvmArgs("--enable-native-access=ALL-UNNAMED")
 }
 
 configure<com.diffplug.gradle.spotless.SpotlessExtension> {
